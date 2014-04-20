5c449e16e01badf0db65f2a368a496eca8c08c90
SolidBuild <SolidBuild@yandex.com>
2014-04-20 21:55:04 +0300

    Added js for sending invites to the server

--- src/documents/js/script.js
+++ src/documents/js/script.js
@@ -1,4 +1,30 @@
 /* Your scripts go here */
 $(document).ready(function(){
 	$("#sticker").sticky({topSpacing:0, wrapperClassName: "hold", className:"fixed"});
+	
+	$('.invite-form #btnGetInvite').click(function(e){
+		e.preventDefault();
+
+		var email = $('.invite-form #txtEmail').val();
+		var atIndex = email.indexOf('@');
+		var lastDotIndex = email.lastIndexOf('.');
+
+		if(email && 0 <= atIndex && atIndex <= lastDotIndex){
+		    var request = $.ajax({
+		                            url: "https://api.solidbuild.net/v1/invites",
+		                            data: JSON.stringify({email:email}),
+		                            type: "POST",
+		                            contentType : "application/json;charset=UTF-8",
+		                        });
+
+		    $('.invite-form #btnGetInvite').html('Sending...').attr('disabled', 'disabled');
+
+		    request.done(function(e){
+		        $('.invite-form #btnGetInvite').html('Get invite').removeAttr('disabled');
+		    });
+		}
+
+		return false;
+	});
+
 });
