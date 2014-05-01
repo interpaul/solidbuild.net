af660a0092b8f0e4b49e963f809382845c67054b
SolidBuild <SolidBuild@yandex.com>
2014-05-01 11:59:38 +0300

    Updated google analytics

--- src/documents/js/script.js
+++ src/documents/js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	$('.invite-form #btnGetInvite').click(function(e){
 		e.preventDefault();
 
-		_gaq.push(['_trackEvent', 'button', 'clicked', 'get invite',, 'true']);
+		ga.push(['_trackEvent', 'button', 'clicked', 'get invite',, 'true']);
 
 		var email = $('.invite-form #txtEmail').val();
 		var atIndex = email.indexOf('@');
